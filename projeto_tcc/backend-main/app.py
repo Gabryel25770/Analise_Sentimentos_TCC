7f57a2e34d1e74e34a50f5e2b4229a4378a8cce1
Gabryel25770 <gabryel-lourenco@hotmail.com>
2025-05-11 23:08:57 -0300

    changes in CORS, app.py

--- projeto_tcc/backend-main/app.py
+++ projeto_tcc/backend-main/app.py
@@ -10,7 +10,11 @@ from collections import defaultdict
 
 # --- Flask App ---
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "http://127.0.0.1:5500"}}, supports_credentials=True)
+CORS(app, resources={r"/*": {"origins": [
+    "http://127.0.0.1:5500", 
+    "https://frontend-main-orcin.vercel.app"
+]}}, supports_credentials=True)
+
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
