97ef1163a89b8e6e7e3d1e66795e4b3e900db431
Gabryel <gabryel-lourenco@hotmail.com>
2025-05-13 20:43:55 -0300

    removing invalid character

--- projeto_tcc/backend-main/app.py
+++ projeto_tcc/backend-main/app.py
@@ -212,4 +212,4 @@ def dashboard_data():
 # --- Início ---
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port)
